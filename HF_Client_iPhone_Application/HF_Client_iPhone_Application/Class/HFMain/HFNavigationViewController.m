31cec95e2a88f838921b7e7aac4790757a13cfe4
陈炳桦 <chenbinghua@chenbinghuademacbook-pro.local>
2018-01-04 11:48:59 +0800

    最新代码

--- HF_Client_iPhone_Application/HF_Client_iPhone_Application/Class/HFMain/HFNavigationViewController.m
+++ HF_Client_iPhone_Application/HF_Client_iPhone_Application/Class/HFMain/HFNavigationViewController.m
@@ -17,7 +17,7 @@
 
 + (void)initialize
 {
-    // 当导航栏用在HFNavigationController中, appearance设置才会生效
+    // 当导航栏用在HFNavigationController中, appearance设置才会生效 ce
     UINavigationBar *bar = [UINavigationBar appearanceWhenContainedIn:[self class], nil];
 //    UINavigationBar *bar = [UINavigationBar appearance];
     [bar setTitleTextAttributes:@{NSFontAttributeName : [UIFont boldSystemFontOfSize:20]}];
